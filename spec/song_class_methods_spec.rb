019fcc12389befb1efe76e7e3318228c845277bf
shannon-nabors <shannon.nabors@gmail.com>
2019-01-10 17:39:48 -0500

    Done

--- spec/song_class_methods_spec.rb
+++ spec/song_class_methods_spec.rb
@@ -55,7 +55,7 @@ describe "Song Class Methods" do
 
       expect(song_1).to eq(song_2)
     end
-    
+
     it 'creates a new Song object with the provided title if one doesn\'t already exist' do
       blank_space = Song.find_by_name("Blank Space")
       expect(blank_space).to be(nil)
@@ -81,6 +81,7 @@ describe "Song Class Methods" do
 
       expect(song.name).to eq("For Love I Come")
       expect(song.artist_name).to eq("Thundercat")
+
     end
   end
 
